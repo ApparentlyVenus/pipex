5a55bb123e15af8dc2d13d1de4fa61c2ca618e59
Omar Dana <odana@c1r9s4.42beirut.com>
2025-06-21 22:31:51 +0300

    parsing logic for bonus

---
+++ src/parse_bonus.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odana <odana@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 22:15:31 by odana             #+#    #+#             */
+/*   Updated: 2025/06/21 22:29:07 by odana            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/pipex_bonus.h"
+#include "../lib/inc/libft.h"
+
+t_node	*parse_args(int argc, char **argv)
+{
+	int	mode;
+
+	mode = parsing_mode(argc, argv);
+	if (mode == 1)
+		return (parse_standard_args(argc, argv));
+	else if (mode == 2)
+		return (parse_heredoc_args(argc, argv));
+	else
+		return (NULL);
+}
+
+t_node	*parse_standard_args(int argc, char **argv)
+{
+	t_node	*current;
+	t_node	*head;
+	t_node	*out_node;
+
+	head = create_infile_node(argv);
+	if (!head)
+		return (NULL);
+	current = parse_sequence(head, argc, argc, 2);
+	if (!current)
+		return (NULL);
+	out_node = create_node(NODE_OUTFILE);
+	if (!out_node)
+		return (free_node_list(head), NULL);
+	out_node->value = ft_strdup(argv[argc - 1]);
+	if (!out_node->value)
+		return (free_node_list(head), NULL);
+	current->next = out_node;
+	return (head);
+}
+
+t_node	*parse_heredoc_args(int argc, char **argv)
+{
+	t_node	*head;
+	t_node	*current;
+	t_node	*out_node;
+
+	head = create_node(NODE_HEREDOC);
+	if (!head)
+		return (NULL);
+	head->value = ft_strdup(argv[2]);
+	if (!head->value)
+		return (free_node_list(head), NULL);
+	current = parse_sequence(head, argc, argv, 3);
+	if (!current)
+		return (NULL);
+	out_node = create_node(NODE_OUTFILE);
+	if (!out_node)
+		return (free_node_list(head), NULL);
+	out_node->value = ft_strdup(argv[argc - 1]);
+	if (!out_node->value)
+		return (free_node_list(head), NULL);
+	return (current->next = out_node, head);
+}
+
+t_node	*parse_sequence(t_node *head, int argc, char **argv, int i)
+{
+	t_node	*current;
+	t_node	*new_node;
+	int		cmd;
+
+	current = head;
+	cmd = 1;
+	while (i < argc - 1)
+	{
+		new_node = NULL;
+		if (cmd)
+			new_node = parse_cmd_node(current, argv, &i, &cmd);
+		else
+			new_node = parse_pipe_node(current, argv, &i, &cmd);
+		if (!new_node)
+			return (free_node_list(head), NULL);
+		current = new_node;
+	}
+	if (cmd)
+		return (free_node_list(head), NULL);
+	return (current);
+}
---
+++ src/parse_utils_bonus.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_utils_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odana <odana@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 22:28:41 by odana             #+#    #+#             */
+/*   Updated: 2025/06/21 22:29:51 by odana            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/pipex_bonus.h"
+#include "../lib/inc/libft.h"
+
+t_node	*create_node(t_node_type type)
+{
+	t_node	*node;
+
+	node = malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->type = type;
+	node->value = NULL;
+	node->args = NULL;
+	node->next = NULL;
+	return (node);
+}
+
+int	parsing_mode(int argc, char **argv)
+{
+	if (argc < 5)
+		return (0);
+	if (ft_strcmp(argv[1], "here_doc") == 0)
+	{
+		if (argc < 6)
+			return (0);
+		return (2);
+	}
+	else
+		return (1);
+}
+
+t_node	*parse_cmd_node(t_node *current, char **argv, int *i, int *cmd)
+{
+	char	**cmd_args;
+	t_node	*cmd_node;
+
+	if (ft_strcmp(argv[*i], "|") == 0)
+		return (NULL);
+	cmd_args = ft_split(argv[*i], ' ');
+	if (!cmd_args)
+		return (NULL);
+	cmd_node = create_node(NODE_CMD);
+	if (!cmd_node)
+		return (free_split(cmd_args), NULL);
+	return (cmd_node->args = cmd_args, current->next = cmd_node,
+		*cmd = 0, (*i)++, cmd_node);
+}
+
+t_node	*parse_pipe_node(t_node *current, char **argv, int *i, int *cmd)
+{
+	t_node	*pipe_node;
+
+	if (ft_strcmp(argv[*i], "|") != 0)
+		return (NULL);
+	pipe_node = create_node(NODE_PIPE);
+	if (!pipe_node)
+		return (NULL);
+	current->next = pipe_node;
+	return (*cmd = 1, (*i)++, pipe_node);
+}
+
+t_node	*create_infile_node(char **argv)
+{
+	t_node	*head;
+
+	head = create_node(NODE_INFILE);
+	if (!head)
+		return (NULL);
+	head->value = ft_strdup(argv[1]);
+	if (!head->value)
+		return (free_node_list(head), NULL);
+	return (head);
+}
