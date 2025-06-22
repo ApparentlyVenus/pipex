6eb940dcd710c177b678e0e61f7bfb080dba6b53
Omar Dana <odana@c1r9s4.42beirut.com>
2025-06-22 03:04:50 +0300

    completed pipex bonus header file

--- inc/pipex_bonus.h
+++ inc/pipex_bonus.h
@@ -6,7 +6,7 @@
 /*   By: odana <odana@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 11:23:36 by odana             #+#    #+#             */
-/*   Updated: 2025/06/21 22:59:11 by odana            ###   ########.fr       */
+/*   Updated: 2025/06/22 01:16:31 by odana            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,9 @@
 # include <fcntl.h>
 # include <sys/wait.h>
 
-/*
- * Node types for the linked list representing the pipeline:
- *  - NODE_INFILE:      input file
- *  - NODE_HEREDOC:     here-doc limiter
- *  - NODE_CMD:         a command with its args[]
- *  - NODE_PIPE:        explicit pipe delimiter
- *  - NODE_OUTFILE:     output file (truncate)
- *  - NODE_OUT_APPEND:  output file (append, e.g. in heredoc mode)
- */
+# include "../lib/inc/libft.h"
+# include "../lib/inc/get_next_line.h"
+
 typedef enum e_node_type
 {
 	NODE_INFILE,
@@ -35,18 +29,8 @@ typedef enum e_node_type
 	NODE_CMD,
 	NODE_PIPE,
 	NODE_OUTFILE,
-	NODE_OUT_APPEND
 }	t_node_type;
 
-/*
- * Linked-list node for each element/token in the pipeline description.
- *  - type: one of t_node_type
- *  - value: for infile/outfile filenames or heredoc limiter
- *  	(NULL for NODE_CMD and NODE_PIPE)
- *  - args: NULL-terminated array of strings for exec 
- * 		(only for NODE_CMD; NULL otherwise)
- *  - next: pointer to next node
- */
 typedef struct s_node
 {
 	t_node_type		type;
@@ -66,8 +50,16 @@ t_node	*parse_pipe_node(t_node *current, char **argv, int *i, int *cmd);
 t_node	*create_infile_node(char **argv);
 int		parsing_mode(int argc, char **argv);
 void	free_split(char **split);
+void	free_node_list(t_node *head);
 
 // execution logic
-void	exec_pipeline(t_node *head, char **envp);
+void	exec_pipeline(t_node *head, char **envp, int fd[2]);
+void	process_infile(t_node *infile);
+void	process_outfile(t_node *outfile);
+void	process_child(t_node *node, int prev_fd, int fd[2], char **envp);
+void	handle_heredoc(t_node	*heredoc);
+void	exec_cmd(t_node *node, char **envp);
+char	*find_path(char *cmd, char **envp);
+void	perror_exit(char *msg);
 
 #endif
